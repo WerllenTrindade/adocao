acea2ea305f47499e9a9c729bb6ad36cbdb9b198
WerllenTrindade <werllentrindade@hotmail.com>
2024-08-13 20:27:01 -0300

    feat: add package react-navigation-stack

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
+import { Home } from './src/screens/Home';
+
+const Stack = createNativeStackNavigator();
 
 export function App() {
   return (
     <View>
-      <Text>Texto</Text>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
